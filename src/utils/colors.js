8cd73783e43c1b8eb65aa515882b50b40e79ee91
Juan <pront23@gmail.com>
2021-06-26 01:25:46 +0900

    added utility files for sizes and colors

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const primaryColors = {
+  lightBlue: '#3E76CB',
+  mediumBlue: '#21539D',
+  darkBlue: '#0D2F62',
+  black: '#04152F',
+  gray: '#C3C9D0',
+  lightGray: '#EDF1F6',
+  ultraLightGray: '#F9FAFC',
+  white: '#FFFFFF',
+};
