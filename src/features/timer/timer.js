fd1ff7b21e672e0256bbc78d95e1271004617b1e
Juan <pront23@gmail.com>
2021-06-27 01:42:45 +0900

    completed timer functions

--- src/features/timer/timer.js
+++ src/features/timer/timer.js
@@ -1,21 +1,70 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-import { Countdown } from '../../components/countdown';
+import { View, StyleSheet, Text, Vibration, Platform } from 'react-native';
+import { ProgressBar } from 'react-native-paper';
+import { useKeepAwake } from 'expo-keep-awake';
+
 import { primaryColors } from '../../utils/colors';
 import { spacing } from '../../utils/sizes';
+
+import { Countdown } from '../../components/countdown';
 import { RoundedButton } from '../../components/roundedButton';
 
-export const Timer = ({ focusSubject }) => {
+import { Timing } from '../timing/timing';
+
+const DEFAULT_TIME = 0.1;
+
+export const Timer = ({ focusSubject, onTimerEnd }) => {
+  useKeepAwake();
+  const [minutes, setMinutes] = useState(DEFAULT_TIME);
   const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(1);
+
+  const onProgress = progress => {
+    setProgress(progress);
+  };
+
+  const vibrate = () => {
+    if (Platform.OS === 'ios') {
+      const interval = setInterval(() => Vibration.vibrate(), 1000);
+      setTimeout(() => clearInterval(interval), 10000);
+    } else {
+      Vibration.vibrate(10000);
+    }
+  };
+
+  const onEnd = () => {
+    vibrate();
+    setMinutes(DEFAULT_TIME);
+    setProgress(1);
+    setIsStarted(false);
+    onTimerEnd();
+  };
+
+  const changeTime = min => {
+    setMinutes(min);
+    setProgress(1);
+    setIsStarted(false);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
-        <Countdown isPaused={!isStarted} />
+        <Countdown
+          minutes={minutes}
+          isPaused={!isStarted}
+          onProgress={onProgress}
+          onEnd={onEnd}
+        />
       </View>
       <View>
         <Text style={styles.title}>Focusing on:</Text>
         <Text style={styles.task}>{focusSubject}</Text>
+        <View style={styles.progressContainer}>
+          <ProgressBar progress={progress} style={styles.progressBar} />
+        </View>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Timing onChangeTime={changeTime} />
       </View>
       <View style={styles.buttonWrapper}>
         {isStarted ? (
@@ -49,8 +98,16 @@ const styles = StyleSheet.create({
   },
   buttonWrapper: {
     flex: 0.3,
+    flexDirection: 'row',
     padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  progressContainer: {
+    paddingTop: spacing.sm,
+  },
+  progressBar: {
+    color: primaryColors.black,
+    height: 10,
   },
 });
