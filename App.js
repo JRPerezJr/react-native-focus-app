fd8114da685a8c2f70d83956cbe2e2ea89a73ead
Juan <pront23@gmail.com>
2021-06-26 21:29:44 +0900

    added stop start btn

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Focus } from './src/features/focus/focus';
+import { Timer } from './src/features/timer/timer';
 import { primaryColors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
 
 export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
   return (
     <View style={styles.container}>
       {focusSubject ? (
-        <Text>I have a subject</Text>
+        <Timer focusSubject={focusSubject} />
       ) : (
         <Focus addSubject={setFocusSubject} />
       )}
@@ -22,6 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
     backgroundColor: primaryColors.mediumBlue,
   },
 });
